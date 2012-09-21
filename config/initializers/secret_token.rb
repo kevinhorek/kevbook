99163ef7c6536c37140926b9f695aeebce58afc7
Kevin Horek <kevin@inkdit.com>
2012-09-21 11:31:23 -0600

    first commit of kevbook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kevbook::Application.config.secret_token = '1543aef3c3f31e35cc7719404edcaefedeeceaa8395dace784bac5050e27c1afb29346433414e7c639d235623fb701075d53a4303ddba4919bfcd67301eb162c'
